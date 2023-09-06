5b12e1f73ee12b177d372f5cf4f5be54fc738ed1
jinjoolee1 <jinjoolee126@gmail.com>
2023-09-06 12:58:55 -0500

    homescreen.jsx fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,12 @@ const Header = () => {
     <header>
       <h2>Devmountain Eatery</h2>
       <nav>
+        <Link to="">
           <button>Home</button>
+          </Link>
+          <Link to="/newRecipe">
           <button>Add Recipe</button>
+          </Link>
       </nav>
     </header>
   );
