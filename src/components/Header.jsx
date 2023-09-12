924b192728205ab5059db7b0c79dc357264bc8fe
jinjoolee1 <jinjoolee126@gmail.com>
2023-09-12 12:28:19 -0500

    progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BiSearchAlt2 } from "react-icons/bi";
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
@@ -6,11 +9,11 @@ const Header = () => {
       <h2>Devmountain Eatery</h2>
       <nav>
         <Link to="">
-          <button>Home</button>
-          </Link>
-          <Link to="/newRecipe">
-          <button>Add Recipe</button>
-          </Link>
+          <button className={styles.nav_btn}>Home</button>
+        </Link>
+        <Link to="/newRecipe">
+          <button className={styles.nav_btn}>Add Recipe</button>
+        </Link>
       </nav>
     </header>
   );
